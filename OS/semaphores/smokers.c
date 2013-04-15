aff01fb7b5b48645a269210a976ad2a085e9a4b8
Ryan Brosnahan <BrosnahanRyan@gmail.com>
2013-04-14 22:03:14 -0400

    smokers.c now has threads that do stuff and semaphores that do stuff. hangs

--- OS/semaphores/smokers.c
+++ OS/semaphores/smokers.c
@@ -4,43 +4,62 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdbool.h>
+#include <errno.h>
 
 #define numSmokers 3
 
 // Semaphores for the agent and the three items
 sem_t agent;
-sem_t tobacco;
-sem_t match;
-sem_t paper;
+sem_t tobaccoMatch;
+sem_t matchPaper;
+sem_t paperTobacco;
 
-bool isTobacco;
-bool isPaper;
-bool isMatch;
-
+// Smoker with paper
 void* paperSmoker(void* arg) {
-
+printf("Smoker with paper waiting to smoke\n");
     while(1) {
         nanosleep((struct timespec[]){{0, rand() % 200000000}}, NULL);
         printf("Smoker with paper waiting to smoke\n");
+
+        sem_wait(&tobaccoMatch);
+        printf("Smoker with paper received other items. Now smoking... \n");
+
+        sem_post(&agent);
+
     }
+
+    return NULL;
 
 }
 
-void* matchSmoker(void* arg) {
-
-    while(1) {
-        nanosleep((struct timespec[]){{0, rand() % 200000000}}, NULL);
-        printf("Smoker with match waiting to smoke\n");
-    }
-
-}
-
+// Smoker with tobacco
 void* tobaccoSmoker(void* arg) {
 
     while(1) {
         nanosleep((struct timespec[]){{0, rand() % 200000000}}, NULL);
         printf("Smoker with tobacco waiting to smoke\n");
+
+        sem_wait(&matchPaper);
+        printf("Smoker with tobacco received other items. Now smoking... \n");
+
+        sem_post(&agent);
     }
+    return NULL;
+}
+
+// Smoker with match
+void* matchSmoker(void* arg) {
+
+    while(1) {
+        nanosleep((struct timespec[]){{0, rand() % 200000000}}, NULL);
+        printf("Smoker with match waiting to smoke\n");
+
+        sem_wait(&paperTobacco);
+        printf("Smoker with match received other items. Now smoking... \n");
+
+        sem_post(&agent);
+    }
+    return NULL;
 
 }
 
@@ -48,18 +67,23 @@ int main() {
 
     // Initialize the semaphores
     sem_init(&agent, 0, 1);
-    sem_init(&tobacco, 0, 0);
-    sem_init(&match, 0, 0);
-    sem_init(&paper, 0, 0);
+    sem_init(&tobaccoMatch, 0, 0);
+    sem_init(&matchPaper, 0, 0);
+    sem_init(&paperTobacco, 0, 0);
 
     // Seeding the RNG for the agent
     srand(time(NULL));
 
     // Create threads of smokers
     pthread_t smokers[numSmokers];
-    pthread_create(&smokers[0], NULL, paperSmoker, NULL);
-    pthread_create(&smokers[0], NULL, matchSmoker, NULL);
-    pthread_create(&smokers[0], NULL, tobaccoSmoker, NULL);
+    if(pthread_create(&smokers[0], NULL, paperSmoker, NULL) == EAGAIN)
+        perror("Insufficent resources to create thread\n");
+
+    if(pthread_create(&smokers[1], NULL, matchSmoker, NULL) == EAGAIN)
+        perror("Insufficent resources to create thread\n");
+
+    if(pthread_create(&smokers[2], NULL, tobaccoSmoker, NULL) == EAGAIN)
+        perror("Insufficent resources to create thread\n");
 
 
     // The "Agent"
@@ -68,20 +92,24 @@ int main() {
 
         // Randomly generate 2 items
         int combination = rand() % 3;
-        if (combination == 0) {
-            isMatch = 1;
-            isPaper = 1;
-        }
-        else if(combination == 1) {
-            isMatch = 1;
-            isTobacco = 1;
-        }
-        else {
-            isTobacco = 1;
-            isPaper = 1;
-        }
+
+        if (combination == 0)
+            sem_post(&tobaccoMatch);
+
+        else if(combination == 1)
+            sem_post(&matchPaper);
+
+        else
+            sem_post(&paperTobacco);
+
+
 
         break;
     }
+
+    for (int i = 0; i < numSmokers; ++i)
+        pthread_join(smokers[i], NULL);
+
+    printf("All threads stopped, exit success\n");
 
 }
