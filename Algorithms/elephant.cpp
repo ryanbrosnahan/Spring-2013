e1ad3c297dda6a1311f0e5b1bdc4dd737e981cac
Ryan Brosnahan <BrosnahanRyan@gmail.com>
2013-05-03 15:48:21 -0400

    starting last algo project

---
+++ Algorithms/elephant.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <list>
+#include <vector>
+
+struct elephant {
+    int weight;
+    int iq;
+};
+
+std::vector<elephant> elephants;
+
+
+int main() {
+    return 0;
+}
+
