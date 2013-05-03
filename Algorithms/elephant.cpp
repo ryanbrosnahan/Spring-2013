061529319684366cafb36df247f72ef507be3127
Ryan Brosnahan <BrosnahanRyan@gmail.com>
2013-05-03 19:22:24 -0400

    getLongest method now generates a vector of vectors that represent arrows for a DAG of the nodes

--- Algorithms/elephant.cpp
+++ Algorithms/elephant.cpp
@@ -12,50 +12,85 @@ struct elephant {
 
 std::vector<elephant> elephants;
 
+/*
+  Gets the data from the file and loads it into a global vector, elephants.
+ */
 void getData() {
 
     std::ifstream infile("e.in");
 
     int i = 1;
-    while(infile.good()) {
+    int tempw;
+    int tempiq;
+    while(infile >> tempw >> tempiq) {
         elephant tempEle;
-        infile >> tempEle.weight >> tempEle.iq;
+        tempEle.weight = tempw;
+        tempEle.iq = tempiq;
         tempEle.index = i;
         elephants.push_back(tempEle);
         i++;
     }
 }
 
+/*
+  For testing purposes, lists all elements that were read form e.in
+ */
 void printV() {
     for(std::vector<elephant>::iterator i = elephants.begin(); i != elephants.end(); ++i)
         std::cout << i->index << " weight: " << i->weight << " IQ: " << i->iq << std::endl;
 }
 
-bool compare(elephant A, elephant B) {
-    if (A.weight == B.weight)
-        return A.iq < B.iq;
-    if (A.weight < B.weight)
-        return 0;
-    return 1;
+/*
+  Used to compare elephants. returns 1 if A > B
+ */
+bool greaterThan(elephant A, elephant B) {
+    return ( (A.weight > B.weight) && (A.iq < B.iq) );
 }
 
-void printSV(std::vector<elephant> elephants) {
-    std::sort(elephants.begin(), elephants.end(), compare);
+void printSV(std::vector< std::vector<elephant> > outer) {
 
-    for(std::vector<elephant>::iterator i = elephants.begin(); i != elephants.end(); ++i)
-        std::cout << i->index << " weight: " << i->weight << " IQ: " << i->iq << std::endl;
+    for(int i = 0; i < outer.size(); ++i) {
+        std::cout << outer[i][0].index << " weight: " << outer[i][0].weight << " IQ: " << outer[i][0].iq << std::endl;
+
+        for(int j = 1; j < outer[i].size(); ++j)
+            std::cout << "  ---->  " << outer[i][j].index << " weight: " << outer[i][j].weight << " IQ: " << outer[i][j].iq << std::endl;
+    }
 }
 
 
 void getLongest() {
+    std::vector< std::vector<elephant > >  forwardList;
+
+    // the outer part of forwardList
+    for(int i = 0; i < elephants.size(); ++i) {
+
+        // the inner part of forwardList
+        std::vector<elephant> tempInner;
+        tempInner.push_back(elephants[i]);
+
+        // for every elephant
+        for(int j = 0; j < elephants.size(); ++j) {
+
+            if (i == j) continue;
+
+            // if the current elephant is larger than the first
+            if ( greaterThan(tempInner.front(), elephants[j]) )
+                tempInner.push_back(elephants[j]);
+        }
+
+        forwardList.push_back(tempInner);
+    }
+
+    printSV(forwardList);
+
+
 
 }
 
 
 int main() {
     getData();
-    printV();
-    //printSV(elephants);
+    getLongest();
     return 0;
 }
 
